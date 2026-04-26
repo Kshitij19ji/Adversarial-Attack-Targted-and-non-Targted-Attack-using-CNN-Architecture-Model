c8887cee571bfc0fc91c2301f9a754c85f8033b9
Kshitij19ji <kshitijsinha261@gmail.com>
2026-04-27 00:44:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Adversarial Attack Project - IIT Bhilai")
+
+print("This project includes:")
+print("- FGSM Attack")
+print("- PGD Attack")
+print("- CW Attack")
+print("- Universal Perturbation")
+
+print("\nModels Used:")
+print("VGG, ResNet, MobileNet, EfficientNet")
+
+print("\nThis is the main entry file of the project")
